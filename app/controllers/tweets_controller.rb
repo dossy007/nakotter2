192b74dafe4b92e8e5af93e08cbf11eb567d14e5
dossy <fedox.toshi@gmail.com>
2018-03-22 11:32:22 +0000

    nekotter2完成

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,6 +1,20 @@
 class TweetsController < ApplicationController
     
     def index
+        @tweets = Tweet.all
+    end
+    
+    def new
+        
+    end
+    
+    def create
+        Tweet.create(params_create)
+    end
+    
+    private
+    def params_create
+        params.permit(:text)
     end
     
 end
